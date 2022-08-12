1243900f9b96b6f8c74cc79db5a707ae31be4377
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-12 20:44:28 +0530

    Modified: checked attendace of employee

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,7 +2,18 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
 
+    static final int IS_PRESENT = 1;
+    
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation");
+        int attendance = (int) (Math.floor(Math.random()*10))%2;
+        System.out.println(attendance);
+        if(attendance == IS_PRESENT){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
     }
 }
