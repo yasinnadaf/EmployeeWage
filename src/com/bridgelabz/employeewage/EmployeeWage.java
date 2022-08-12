4a3c0243e3734f23eb43034d2109f0e5f6fe6040
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-12 20:58:25 +0530

    Modified: calculated daily wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,7 +3,9 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
 
     static final int IS_PRESENT = 1;
-    
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation");
@@ -11,6 +13,8 @@ public class EmployeeWage {
         System.out.println(attendance);
         if(attendance == IS_PRESENT){
             System.out.println("Employee is present");
+            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee daily wage is: "+dailyWage);
         }
         else{
             System.out.println("Employee is absent");
