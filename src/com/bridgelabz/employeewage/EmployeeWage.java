15c5ea165b4375a840d8ede047e0746bcd83889c
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-12 21:11:57 +0530

    Modified: part time hour

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,17 +3,24 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
 
     static final int IS_PRESENT = 1;
+    static final int IS_PART_TIME = 2;
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation");
-        int attendance = (int) (Math.floor(Math.random()*10))%2;
-        System.out.println(attendance);
+        int attendance = (int) (Math.floor(Math.random()*10))%3;
+//        System.out.println(attendance);
         if(attendance == IS_PRESENT){
-            System.out.println("Employee is present");
+            System.out.println("Employee is working full time");
             int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee daily wage is: "+dailyWage);
+        }
+        else if (attendance == IS_PART_TIME){
+            System.out.println("Employee is working part time");
+            int dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
             System.out.println("Employee daily wage is: "+dailyWage);
         }
         else{
