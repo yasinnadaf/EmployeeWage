1bb5d5d356c6246d7f0f795635b9748efbc7c6a4
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-17 22:51:24 +0530

    modified: multiple companies

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -4,15 +4,11 @@ public class EmployeeWage {
 
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
-    static final int PART_TIME_HOUR = 4;
-    static final int WORKING_DAY_PER_MONTH = 20;
-    static final int TOTAL_WORK_HOUR = 100;
 
 
-    static void calculateWage(){
+    static void calculateWage(String company, int wagePerHr, int daysPerMonth,int workHrPerMonth){
         System.out.println("Welcome to Employee Wage Computation");
+         int empHrs;
          int totalHours = 0;
          int day = 0;
          int totalWage = 0;
@@ -20,33 +16,39 @@ public class EmployeeWage {
 
 
 
-        while((totalHours< TOTAL_WORK_HOUR) && (day<WORKING_DAY_PER_MONTH )){
+        while((totalHours< workHrPerMonth) && (day<daysPerMonth )){
             int attendance = (int)(Math.floor(Math.random()*10))%3;
 
             switch (attendance) {
 
                 case IS_FULL_TIME:
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    totalHours += FULL_DAY_HOUR;
+                    empHrs = 8;
                     break;
 
                 case IS_PART_TIME:
-                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    totalHours += PART_TIME_HOUR;
+                    empHrs = 4;
                     break;
                 default:
+                    empHrs = 0;
                     break;
             }
-            totalWage += dailyWage;
+            totalHours+=empHrs;
+            dailyWage = wagePerHr*empHrs;
+            totalWage+=dailyWage;
             day++;
+            System.out.println("Day "+day+" Emp work hour "+empHrs+ " wage is "+dailyWage);
+
         }
         System.out.println("Total work hour is: "+totalHours);
         System.out.println("Total work days are: "+day);
-        System.out.println("Total wage is: "+totalWage);
+        System.out.println(" wage is: "+totalWage);
+        System.out.println("Total Wage of company "+company+ " is "+ totalWage);
+
 
     }
 
     public static void main(String[] args) {
-        calculateWage();
+        calculateWage("Flipkart",10,20,100);
+        calculateWage("Amazon",12,22,90);
     }
 }
